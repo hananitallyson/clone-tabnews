049aad3cc0de9a388744d24778fd352d15e37bcd
hananitallyson <tallysonhanani@gmail.com>
2023-12-19 19:43:18 -0300

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,10 +4,12 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const databaseVersion = (await database.query("SHOW server_version;")).rows[0]
     .server_version;
+  const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnections = (
-    await database.query(
-      "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-    )
+    await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    })
   ).rows[0].count;
   const databaseMaxConnections = (await database.query("SHOW max_connections;"))
     .rows[0].max_connections;
